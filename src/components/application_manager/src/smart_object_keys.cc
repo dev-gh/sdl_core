ebe26d155b4137dbad05ee89d2ab8b850d2c4210
Alexander Kutsan <akutsan@luxoft.com>
2017-08-10 14:07:09 +0300

    Implementation of Remote Control plugin
    Stype changes after integration
    
    Fix policy handler remote unit tests

--- src/components/application_manager/src/smart_object_keys.cc
+++ src/components/application_manager/src/smart_object_keys.cc
@@ -139,6 +139,7 @@ const char* system_capability_type = "systemCapabilityType";
 const char* system_capabilities = "systemCapabilities";
 const char* navigation_capability = "navigationCapability";
 const char* phone_capability = "phoneCapability";
+const char* device_rank = "deviceRank";
 
 // PutFile
 const char* sync_file_name = "syncFileName";
